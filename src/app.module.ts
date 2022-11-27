d65ed5d479d251f0b2b839d55979eea4222146aa
Jeremy Moyson <moyson.jeremy@gmail.com>
2022-11-27 14:02:19 +0100

    Setup prisma models, create prisma module and service global and add it to auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { SubscriptionModule } from './subscription/subscription.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, SubscriptionModule],
+  imports: [AuthModule, UserModule, SubscriptionModule, PrismaModule],
 })
 export class AppModule {}
