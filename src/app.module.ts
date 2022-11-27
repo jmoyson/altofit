db50145b22a75c84239fc47fa3059b1f2062d04b
Jeremy Moyson <moyson.jeremy@gmail.com>
2022-11-27 13:26:54 +0100

    Create modules we will use

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, SubscriptionModule],
 })
 export class AppModule {}
