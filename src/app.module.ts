b81f418a36b85f3ca01138e85dca829e546db08b
Jeremy Moyson <moyson.jeremy@gmail.com>
2022-11-27 14:30:19 +0100

    Setup nestjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, SubscriptionModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    SubscriptionModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://alto_user:alto_password@localhost:5432/alto_db?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
