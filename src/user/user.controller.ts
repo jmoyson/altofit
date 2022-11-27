d506960e2bc65574bc52b45fa16ac6e3fd45b75e
Jeremy Moyson <moyson.jeremy@gmail.com>
2022-11-27 15:30:00 +0100

    Setup CRUD for subscription and swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,9 +2,11 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { ApiTags } from '@nestjs/swagger';
 
 @UseGuards(JwtGuard)
 @Controller('users')
+@ApiTags('users')
 export class UserController {
   @Get('me')
   getMe(@GetUser() user: User) {
